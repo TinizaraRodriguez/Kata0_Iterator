84bc35276442e522fb5708435cc8f86efc3fa2f5
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2020-12-21 12:23:51 +0000

    Add files via upload

---
+++ Kata0_Iterator/src/kata0_iterator/IteratorReader.java
@@ -0,0 +1,44 @@
+
+package kata0_iterator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+
+public class IteratorReader implements Iterable<String> {
+
+    private final BufferedReader reader;
+
+    public IteratorReader(BufferedReader reader) {
+        this.reader = reader;
+    }
+    
+    @Override
+    public Iterator<String> iterator() {
+        return new Iterator<String>() {
+            String currentLine = readLine();
+            
+            @Override
+            public boolean hasNext() {
+                return currentLine != null;
+            }
+
+            @Override
+            public String next() {
+                String resultLine = currentLine;
+                currentLine = readLine();
+                return resultLine;
+            }
+            
+            private String readLine() {
+                try {
+                    return reader.readLine();
+                } catch (IOException ex) {
+                    return null;
+                }
+            }
+        };
+    }
+    
+}
